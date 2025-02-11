1426614b7cfa3c1dd8d274c59d7d052110ccfe09
khe4oyan <khe4oyan@gmail.com>
2025-02-11 19:02:57 +0400

    .

---
+++ src/redux/slices/filmsSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const filmsSlice = createSlice({
+  name: "filmsSlice",
+
+  initialState: {
+    films: [],
+  },
+
+  reducers: {
+    
+  },
+});
+
+export default filmsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 // libs
 import { configureStore } from "@reduxjs/toolkit";
 
+// slices
+import filmsSlice from './slices/filmsSlice';
+
 const store = configureStore({
   reducer: {
-    
+    films: filmsSlice,
   }
 });
 
